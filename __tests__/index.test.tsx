6678d7078038fbcef8fe5ae33ef5fbb8aaf259a2
Tittoh <tittohb@gmail.com>
2023-02-09 11:59:54 +0300

    Add Heroicons and headless ui

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -7,8 +7,8 @@ describe('Home', () => {
   it('renders a next.js logo', () => {
     render(<Home />);
 
-    const heading = screen.getByRole('img', {
-      name: /Next\.js Logo/i,
+    const heading = screen.getByRole('heading', {
+      name: 'Next.js 13 and Tailwind CSS Template',
     });
 
     expect(heading).toBeInTheDocument();
