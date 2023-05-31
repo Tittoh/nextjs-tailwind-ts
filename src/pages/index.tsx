a70d478eded09abaf80a9f64f54b616dd0fd2cc8
Tittoh <tittohb@gmail.com>
2023-05-31 10:48:22 +0300

    chore: Update ESlint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
-
 import { Dialog } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Head from 'next/head';
+import { useState } from 'react';
 
 const navigation = [
   { name: 'Product', href: '#' },
